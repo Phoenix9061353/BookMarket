f5842d26816301eb9a1ee9b614b0c5d20b763d81
Phoenix Chang <ph90630@gmail.com>
2022-11-15 19:23:55 +0800

    Solved bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const helmet = require('helmet');
+const path = require('path');
 const compression = require('compression');
 
 const authRoute = require('./routes/authRoutes');
